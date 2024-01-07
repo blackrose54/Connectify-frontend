4fea31c535cdf60bec12e1b9f98fa95ad10f6c0a
blackrose <bhakatabhinav54@gmail.com>
2024-01-07 15:27:11 +0530

    fixed unread

--- src/components/SidebarChat.tsx
+++ src/components/SidebarChat.tsx
@@ -2,21 +2,42 @@
 
 import { useSocket } from "@/context/SocketProvider";
 import Image from "next/image";
-import React, { FC} from "react";
+import React, { FC, useEffect } from "react";
 
 interface ChatProps {}
 
 const Chat: FC<ChatProps> = ({}) => {
-  const { Friends, current, setCurrent, chats, Unread, setUnread,online,Recents } =
-    useSocket();
-    const sortedEntries = Array.from(Recents.entries()).sort((a,b)=>b[1]-a[1]);
+  const {
+    Friends,
+    current,
+    setCurrent,
+    chats,
+    Unread,
+    setUnread,
+    online,
+    Recents,
+  } = useSocket();
+  const sortedEntries = Array.from(Recents.entries()).sort(
+    (a, b) => b[1] - a[1]
+  );
+  
+    useEffect(()=>{
+      fetch("/api/chats/update", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Object.fromEntries(Unread.entries())),
+      });
+    },[Unread])
+
   return (
     <div className="containerCustom grow">
       <p className="text-lg border-b-2 p-2">Chats</p>
 
       {sortedEntries.map((entry, key) => {
-        const friend = Friends.find(friend=>friend.id === entry[0])
-        if(!friend) return <></>
+        const friend = Friends.find((friend) => friend.id === entry[0]);
+        if (!friend) return <></>;
         return (
           <div
             key={key}
@@ -24,38 +45,47 @@ const Chat: FC<ChatProps> = ({}) => {
               current === friend ? "bg-slate-200" : ""
             } flex space-x-4 items-center cursor-pointer hover:bg-slate-200 p-4 border-b-2`}
             onClick={async () => {
-              if (current)
+              if (current) {
+                console.log('current')
                 setUnread((prev: Map<string, number>) => {
                   const newMap = new Map(prev);
                   newMap.set(current.id, 0);
                   return newMap;
                 });
-              else
+              } else {
                 setUnread((prev: Map<string, number>) => {
                   const newMap = new Map(prev);
                   newMap.set(friend.id, 0);
                   return newMap;
                 });
+              }
               setCurrent(friend);
 
-              await fetch("/api/chats/update", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(Object.fromEntries(Unread.entries())),
-              });
+              
             }}
           >
-            <div className={`${online && online.has(friend.id) && online.get(friend.id) == true?"image-container-online":"image-container-offline"}  h-10 w-10`}>
-            <Image
-              src={friend.image!}
-              alt="avatar"              
-              fill
-              className={`rounded-full after:bg-white ring-2 ${online && online.has(friend.id) && online.get(friend.id) == true?"ring-green-400":"ring-slate-300"}`}
-            />
+            <div
+            key={friend.id}
+              className={`${
+                online && online.has(friend.id) && online.get(friend.id) == true
+                  ? "image-container-online"
+                  : "image-container-offline"
+              }  h-10 w-10`}
+            >
+              <Image
+                src={friend.image!}
+                alt="avatar"
+                fill
+                className={`rounded-full after:bg-white ring-2 ${
+                  online &&
+                  online.has(friend.id) &&
+                  online.get(friend.id) == true
+                    ? "ring-green-400"
+                    : "ring-slate-300"
+                }`}
+              />
             </div>
-            <div>
+            <div key={friend.id + key}>
               <p>{friend.name}</p>
               <p className="text-xs text-slate-400">
                 {chats.get(friend.id)?.at(-1) ? (
